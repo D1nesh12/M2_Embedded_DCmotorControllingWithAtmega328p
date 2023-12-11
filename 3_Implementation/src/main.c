23765e717fe3295852688d70737c47adf872a876
Dinesh A <102590808+D1nesh12@users.noreply.github.com>
2023-12-11 20:05:45 +0530

    Update main.c

--- 3_Implementation/src/main.c
+++ 3_Implementation/src/main.c
@@ -1,12 +1,7 @@
-/*
- * dcmotor1.c
- *
- * Created: 01-12-2021 15:30:17
- * Author : ROHIT
- */ 
-
 
 #include <avr/io.h>
+
+
 int main(void)
 {
   DDRB=0x00;
@@ -28,12 +23,6 @@ PORTC=0x01;
 
 
 
-/*
- * dcmotor2.c
- *
- * Created: 01-12-2021 15:44:20
- * Author : ROHIT
- */ 
 
 #include <avr/io.h>
 int main(void)
